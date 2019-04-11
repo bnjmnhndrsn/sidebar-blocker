e2cdf2fd5515e4fd41929cc40560eaeb657115e5
Ben Henderson <benjamin.c.henderson@gmail.com>
2019-04-10 23:27:37 -0400

    refactor toggle element javascript

--- popup.js
+++ popup.js
@@ -1,19 +1,27 @@
-let checkbox = document.getElementById('checkbox');
+class Toggle {
+    constructor({initialValue}){
+        this.initialValue = initialValue;
+        this.checkbox = document.getElementById('checkbox');
+        this.flushState();
+        this.bindEvents();
+    }
 
-function bindCheckboxEventHandler(){
-    checkbox.addEventListener('change', function(e){
-        const values = {sidebarDisabled: e.target.checked};
-        console.log(values);
-        chrome.storage.sync.set(values);
-    });
+    bindEvents(){
+        this.checkbox.addEventListener('change', function(e){
+            const values = {sidebarDisabled: e.target.checked};
+            chrome.storage.sync.set(values);
+        });
+    }
+
+    flushState(){
+        if (this.initialValue) {
+            this.checkbox.checked = true;
+        } else {
+            this.checkbox.checked = false;
+        }
+    }
 }
 
 chrome.storage.sync.get('sidebarDisabled', function({sidebarDisabled}) {
-    if (sidebarDisabled) {
-        checkbox.checked = true;
-    } else {
-        checkbox.checked = false;
-    }
-
-    bindCheckboxEventHandler();
+    const toggle = new Toggle({initialValue: sidebarDisabled});
 });
